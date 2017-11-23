dab8bb58410d1fa7e130a4ee39caccb9093ab4f6
Jagroop Singh <geek.jagroop@gmail.com>
2017-11-23 10:23:10 +0530

    Init

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Git Learning</title>
+</head>
+<body>
+  <h1>Hello World !!</h1> 
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa, voluptatibus numquam ipsam non expedita ratione. Ipsum non, quidem iusto debitis sit illum nihil eveniet hic nesciunt, libero eos dolores tenetur.</p>
+</body>
+</html>
